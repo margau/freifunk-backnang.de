1bba7409cc7179d1644c01b76f66336363350381
Patrick Schwarz <ps@p-schwarz.de>
2021-11-30 23:55:48 +0100

    Bugfix Nullvalue

--- assets/js/jquery.dbkcalendar.js
+++ assets/js/jquery.dbkcalendar.js
@@ -13,7 +13,7 @@
         $.each(data, function(date, day) {
             items.push("<tr data-date='" + date + "'><th colspan='3'>" + day.name + "</th><th>" + day.weekday + "</th></tr>");
             $.each(day.events, function(uid, event) {
-		event.description = event.description.replace(/\n/g,"<br>").replace(uri_regex, "<a href='$1' target='_blank'>$1</a>");
+		        if(event.description) event.description = event.description.replace(/\n/g,"<br>").replace(uri_regex, "<a href='$1' target='_blank'>$1</a>");
                 items.push("<tr data-uid='" + uid + "'><td>" + event.datestr + "</td><td>" + event.summary + "</td><td>" + ((event.location)?event.location:'') + "</td><td>" + ((event.description)?event.description:'') + "</td></tr>");
             });
         });
