37bda92753c60aa2d794afca9f6aa6682a5800ab
Patrick Schwarz <ps@p-schwarz.de>
2022-01-10 19:46:17 +0100

    Add location URL replacement and allow ":" in URL

--- assets/js/jquery.dbkcalendar.js
+++ assets/js/jquery.dbkcalendar.js
@@ -6,13 +6,14 @@
 (function($) {
 
     var cal_uri = "https://cloud.hacknang.de/hackcal/";
-    var uri_regex = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/\-_\.]*(\?\S+)?)?)?)/ig
+    var uri_regex = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/\-_\.\:]*(\?\S+)?)?)?)/ig
 
     $.getJSON(cal_uri, function(data) {
 	var items = [];
         $.each(data, function(date, day) {
             items.push("<tr data-date='" + date + "'><th colspan='3'>" + day.name + "</th><th>" + day.weekday + "</th></tr>");
             $.each(day.events, function(uid, event) {
+                if(event.location) event.location = event.location.replace(/\n/g,"<br>").replace(uri_regex, "<a href='$1' target='_blank'>$1</a>");
 		        if(event.description) event.description = event.description.replace(/\n/g,"<br>").replace(uri_regex, "<a href='$1' target='_blank'>$1</a>");
                 items.push("<tr data-uid='" + uid + "'><td>" + event.datestr + "</td><td>" + event.summary + "</td><td>" + ((event.location)?event.location:'') + "</td><td>" + ((event.description)?event.description:'') + "</td></tr>");
             });
